3e74a6dda713fc4f461ef3f6ae1e939252a2e0be
Max <agnusmaximus@berkeley.edu>
2017-03-14 23:41:39 -0700

    Update

--- resnet/src/distributed_train.py
+++ resnet/src/distributed_train.py
@@ -216,7 +216,7 @@ def train(target, cluster_spec):
   compute_R_train_error_time = 0
   loss_value = -1
 
-  checkpoint_save_secs = 60 * 2
+  checkpoint_save_secs = 30
 
   with tf.train.MonitoredTrainingSession(
       master=target, is_chief=is_chief,
