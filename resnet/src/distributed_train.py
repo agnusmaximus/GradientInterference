fdc5b401700375ee230d28c22fd08c0ec3c072ba
Max <agnusmaximus@berkeley.edu>
2017-03-15 21:58:20 -0700

    Update

--- resnet/src/distributed_train.py
+++ resnet/src/distributed_train.py
@@ -325,14 +325,15 @@ def train(target, cluster_spec):
         img_work, label_work = sess.run(variable_batchsize_inputs[1], feed_dict={images:np.zeros([1, 32, 32, 3]), labels: np.zeros([1, 10 if FLAGS.dataset == 'cifar10' else 100])})
         tf.logging.info(str(img_work.shape) + " " + str(label_work.shape))
         worker = i % num_workers
-        tf.logging.info("Assigning example %d to worker %d for computing R..." % (i, worker_id))
+        tf.logging.info("Assigning example %d to worker %d for computing R..." % (i, worker))
+        sys.stdout.flush()
         feed_dict={images:np.zeros([1, 32, 32, 3]), labels: np.zeros([1, 10 if FLAGS.dataset == 'cifar10' else 100])}
         #feed_dict={}
         #feed_dict[work_image_placeholder] = img_work
         #feed_dict[work_label_placeholder] = img_label
         #sess.run([enqueue_image_ops_for_r[i], enqueue_label_ops_for_r[i]], feed_dict=feed_dict)
         tf.logging.info("YO")
-        sess.run([enqueue_label_ops_for_r[i]], feed_dict=feed_dict)
+        sess.run([enqueue_label_ops_for_r[worker]], feed_dict=feed_dict)
       tf.logging.info("Master done distributing examples for computing R...")
     mon_sess.run([unblock_workers_op],feed_dict={images:np.zeros([1, 32, 32, 3]), labels: np.zeros([1, 10 if FLAGS.dataset == 'cifar10' else 100])})
 
