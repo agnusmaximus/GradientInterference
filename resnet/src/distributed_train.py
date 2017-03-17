f97394ca3692465fc8ce9a31ac875c7abd8baa3b
Max <agnusmaximus@berkeley.edu>
2017-03-16 21:53:19 -0700

    Update

--- resnet/src/distributed_train.py
+++ resnet/src/distributed_train.py
@@ -283,7 +283,7 @@ def train(target, cluster_spec):
         R_labels_work_queue.append(data_flow_ops.FIFOQueue(-1, tf.int64, name=name_labels, shared_name=name_labels))
 
       gradient_sums_queue = data_flow_ops.FIFOQueue(-1, tf.float32, name="gradient_sums_queue")
-      sum_of_norms_queue = data_flow_ops.FIFOQueue(-1, tf.float32, name="gradient_sums_queue")
+      sum_of_norms_queue = data_flow_ops.FIFOQueue(-1, tf.float32, name="gradient_norms_queue")
 
     gradient_sum_placeholder = tf.placeholder(tf.float32, shape=(None))
     gradient_sums_enqueue = gradient_sums_queue.enqueue(gradient_sum_placeholder)
