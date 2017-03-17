a78a6686d1f86f996389e756bc1a8270d92a35b6
Max <agnusmaximus@berkeley.edu>
2017-03-17 06:22:10 -0700

    Update

--- resnet/src/distributed_train.py
+++ resnet/src/distributed_train.py
@@ -396,7 +396,7 @@ def train(target, cluster_spec):
       # Dequeue all components
       fd = {images:np.zeros([1, 32, 32, 3]), labels: np.zeros([1, 10 if FLAGS.dataset == 'cifar10' else 100])}
       total_sum_of_norms, total_sum_of_gradients = sess.run([sum_of_norms_dequeue, gradient_sums_dequeue], feed_dict=fd)
-      for i in range(num_workers)-1
+      for i in range(num_workers)-1:
         gsum, gnorm = sess.run([sum_of_norms_dequeue, gradient_sums_dequeue], feed_dict=fd)
         total_sum_of_norms += gnorm
         total_sum_of_gradients += gsum
