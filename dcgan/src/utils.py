4fd69b8a45209a089f7aa2310a1c914390cef8f1
Max <agnusmaximus@berkeley.edu>
2017-03-26 14:15:46 -0700

    Update dcgan

--- dcgan/src/utils.py
+++ dcgan/src/utils.py
@@ -42,12 +42,14 @@ def merge_images(images, size):
   return inverse_transform(images)
 
 def merge(images, size):
+  return images[0]
   h, w = images.shape[1], images.shape[2]
   img = np.zeros((h * size[0], w * size[1], 3))
   for idx, image in enumerate(images):
     i = idx % size[1]
     j = idx // size[1]
     img[j*h:j*h+h, i*w:i*w+w, :] = image
+
   return img
 
 def imsave(images, size, path):
