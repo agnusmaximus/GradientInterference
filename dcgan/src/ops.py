bb17458fbb3e2f11d026d7928b4b9966441201fd
Max <agnusmaximus@berkeley.edu>
2017-03-26 01:14:47 -0700

    Update dcgan

--- dcgan/src/ops.py
+++ dcgan/src/ops.py
@@ -48,8 +48,8 @@ def conv_cond_concat(x, y):
   y_shapes = y.get_shape()
   tf.logging.info(x.shape)
   tf.logging.info(y.shape)
-  #shape = tf.TensorShape([None, int(x_shapes[1]), int(x_shapes[2]), int(y_shapes[3])])
-  ones_v = tf.ones(tf.shape(x))
+  shape = tf.TensorShape([tf.shape(x).dims[0], int(x_shapes[1]), int(x_shapes[2]), int(y_shapes[3])])
+  ones_v = tf.ones(shape)
   #return concat([
     #x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
   return concat([
