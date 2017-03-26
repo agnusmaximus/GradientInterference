b88e9a19cb2bb02827e4a50e573b021b4fba6835
Max <agnusmaximus@berkeley.edu>
2017-03-26 00:51:10 -0700

    Update dcgan

--- dcgan/src/ops.py
+++ dcgan/src/ops.py
@@ -46,9 +46,7 @@ def conv_cond_concat(x, y):
   """Concatenate conditioning vector on feature map axis."""
   x_shapes = x.get_shape()
   y_shapes = y.get_shape()
-  tf.logging.info(x.shape)
-  shape = [x_shapes[0], int(x_shapes[1]), int(x_shapes[2]), int(y_shapes[3])]
-  ones_v = tf.fill(tf.shape(x), 1)
+  ones_v = tf.fill(tf.shape(y), 1)
   #return concat([
     #x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
   return concat([
