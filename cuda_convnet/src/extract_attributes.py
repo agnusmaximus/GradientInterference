b29245deedc1333df395632d629e23a52b2c6ff3
Ubuntu <ubuntu@ip-172-31-32-193.us-west-2.compute.internal>
2017-04-14 04:37:54 +0000

    Update plotting code

--- cuda_convnet/src/extract_attributes.py
+++ cuda_convnet/src/extract_attributes.py
@@ -10,22 +10,20 @@ def override_and_set_tf_flags(run_flags):
     tf_flags = FLAGS.__flags
     assert(type(tf_flags) == type({}))
     for k,v in tf_flags.items():
-        if k in run_flags.keys():            
-            type_of_value = type(tf_flags[k])
-            tf_flags[k] = type_of_value(run_flags[k])
+        if k in run_flags.keys():
+            tf_flags[k] = eval(run_flags[k])
 
     # Sanity check
     for k,v in tf_flags.items():
         if k in run_flags.keys():
             actual_value = tf_flags[k]
-            type_of_value = type(actual_value)
-            expected_value = type_of_value(run_flags[k])
+            expected_value = eval(run_flags[k])
             assert(actual_value == expected_value)
 
 def test_name_function_pair(model, run_flags):
     return 0
 
-def extract_training_accuracy(model_variables, run_flags):
+def extract_training_accuracy(model_variables_materialized, run_flags):
 
     # We try to download data if not already downloaded
     cifar10.maybe_download_and_extract()
@@ -35,7 +33,44 @@ def extract_training_accuracy(model_variables, run_flags):
     # But that's ok since the main flags such as learning rate, whether to use fractional dataset
     # are bools/ints/floats
     override_and_set_tf_flags(run_flags)
+
+    print(FLAGS.__flags)
     
+    # Load data
+    images_train_raw, labels_train_raw, images_test_raw, labels_test_raw = load_cifar_data_raw()
+
+    tf.reset_default_graph()
+
+    # Build the model
+    scope_name = "parameters_1"
+    with tf.variable_scope(scope_name):
+        images = tf.placeholder(tf.float32, shape=(None, cifar10.IMAGE_SIZE, cifar10.IMAGE_SIZE, cifar10.NUM_CHANNELS))
+        labels = tf.placeholder(tf.int32, shape=(None,))
+        logits = cifar10.inference(images)
+        loss_op = cifar10.loss(logits, labels, scope_name)
+        train_op = cifar10.train(loss_op, scope_name)
+        top_k_op = tf.nn.in_top_k(logits, labels, 1)
+
+    # Ops to load model from the variables
+    model_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope_name)
+    assert(len(model_variables) == len(model_variables_materialized.items()))
+
+    placeholder_and_materialized_values = {}
+    load_model_ops = []
+    for variable in model_variables:
+        assert(variable.name in model_variables_materialized.keys())
+        placeholder = tf.placeholder(variable.dtype, shape=variable.shape)
+        placeholder_and_materialized_values[placeholder] = model_variables_materialized[variable.name]
+        load_model_ops.append(tf.assign(variable, placeholder))
+
+    with tf.Session() as sess:
+      tf.initialize_all_variables().run()
+      tf.train.start_queue_runners(sess=sess)
+
+      # Load model from materialized values
+      sess.run([load_model_ops])
+
+      
     
     
     
