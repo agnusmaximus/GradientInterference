840333e982c233a921122b50f65e037cc5d326ad
Max <agnusmaximus@berkeley.edu>
2017-04-22 13:59:30 -0700

    Update resnet single

--- resnet/src/resnet_model.py
+++ resnet/src/resnet_model.py
@@ -123,7 +123,7 @@ class ResNet(object):
     with tf.variable_scope('costs'):
       xent = tf.nn.softmax_cross_entropy_with_logits(
           logits=logits, labels=self.labels)
-      self.cost = tf.reduce_mean(xent, name='xent')
+      self.cost = tf.reduce_sum(xent, name='xent')
       self.cost += self._decay()
 
       tf.summary.scalar('cost', self.cost)
